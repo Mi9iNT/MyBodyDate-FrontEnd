cdcc32d28b314917d8fd61126d15856227a36094
Mi9iNT <72730181+Mi9iNT@users.noreply.github.com>
2023-07-29 19:30:51 +0200

    MàJ Settings.js

--- components/screens/settings/Settings.js
+++ components/screens/settings/Settings.js
@@ -34,128 +34,131 @@ export const Settings = ({navigation}) => {
         <View style={settingsStyles.separator} />
         <View style={settingsStyles.viewScroll}>
           <ScrollView
-          style={settingsStyles.scrollView} // Utilisez le style pour la ScrollView
-          contentContainerStyle={{paddingBottom: 0}}>
-          <TouchableOpacity
-            accessibilityLabel="Notifications"
-            onPress={() => navigation.navigate('Notifications')}>
-            <View style={settingsStyles.notificationItem}>
-              <Text style={settingsStyles.notificationText}>Notifications</Text>
-              <Image
-                style={settingsStyles.arrowIcon}
-                source={require('../../../assets/images/fleche-blue.png')}
-              />
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            accessibilityLabel="Sécurité et vie privée"
-            onPress={() => navigation.navigate('Securite et privee')}>
-            <View style={settingsStyles.notificationItem}>
-              <Text style={settingsStyles.notificationText}>
-                Sécurité & vie privée
-              </Text>
-              <Image
-                style={settingsStyles.arrowIcon}
-                source={require('../../../assets/images/fleche-blue.png')}
-              />
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            accessibilityLabel="Contact et FAQ"
-            onPress={() => navigation.navigate('Contact et FAQ')}>
-            <View style={settingsStyles.notificationItem}>
-              <Text style={settingsStyles.notificationText}>Contact & FAQ</Text>
-              <Image
-                style={settingsStyles.arrowIcon}
-                source={require('../../../assets/images/fleche-blue.png')}
-              />
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            accessibilityLabel="Emplacement"
-            onPress={() => navigation.navigate('Emplacement')}>
-            <View style={settingsStyles.locationItem}>
-              <Text style={settingsStyles.locationText}>Emplacement</Text>
-              <Text style={settingsStyles.locationValue}>Paris, FR</Text>
-              <Image
-                style={settingsStyles.arrowIcon}
-                source={require('../../../assets/images/fleche-blue.png')}
-              />
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            accessibilityLabel="Mode invisible"
-            onPress={() => navigation.navigate('Mode invisible')}>
-            <View style={settingsStyles.modeItem}>
-              <View style={settingsStyles.modeContainer}>
-                <Text style={settingsStyles.modeText}>Mode invisible</Text>
-                <Text style={settingsStyles.modeDescription}>
-                  Visitez des profils incognito
+            style={settingsStyles.scrollView} // Utilisez le style pour la ScrollView
+            contentContainerStyle={{paddingBottom: 0}}>
+            <TouchableOpacity
+              accessibilityLabel="Notifications"
+              onPress={() => navigation.navigate('Notifications')}>
+              <View style={settingsStyles.notificationItem}>
+                <Text style={settingsStyles.notificationText}>
+                  Notifications
                 </Text>
+                <Image
+                  style={settingsStyles.arrowIcon}
+                  source={require('../../../assets/images/fleche-blue.png')}
+                />
               </View>
-              <Image
-                style={settingsStyles.arrowIcon}
-                source={require('../../../assets/images/fleche-blue.png')}
-              />
-            </View>
-          </TouchableOpacity>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            accessibilityLabel="Mode voyage"
-            onPress={() => navigation.navigate('Mode voyage')}>
-            <View style={settingsStyles.modeItem}>
-              <View style={settingsStyles.modeContainer}>
-                <Text style={settingsStyles.modeText}>Mode voyage</Text>
-                <Text style={settingsStyles.modeDescription}>
-                  Pour voir plus de monde. Changez votre localisation.
+            <TouchableOpacity
+              accessibilityLabel="Sécurité et vie privée"
+              onPress={() => navigation.navigate('Securite et privee')}>
+              <View style={settingsStyles.notificationItem}>
+                <Text style={settingsStyles.notificationText}>
+                  Sécurité & vie privée
                 </Text>
+                <Image
+                  style={settingsStyles.arrowIcon}
+                  source={require('../../../assets/images/fleche-blue.png')}
+                />
               </View>
-              <Image
-                style={settingsStyles.arrowIcon}
-                source={require('../../../assets/images/fleche-blue.png')}
-              />
-            </View>
-          </TouchableOpacity>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            accessibilityLabel="Mettre mon compte en pause"
-            onPress={() => navigation.navigate('Mettre en pause')}>
-            <View style={settingsStyles.notificationItem}>
-              <Text style={settingsStyles.notificationText}>
-                Mettre mon compte en pause
-              </Text>
-              <Image
-                style={settingsStyles.arrowIcon}
-                source={require('../../../assets/images/fleche-blue.png')}
-              />
-            </View>
-          </TouchableOpacity>
-        </ScrollView>
+            <TouchableOpacity
+              accessibilityLabel="Contact et FAQ"
+              onPress={() => navigation.navigate('Contact et FAQ')}>
+              <View style={settingsStyles.notificationItem}>
+                <Text style={settingsStyles.notificationText}>
+                  Contact & FAQ
+                </Text>
+                <Image
+                  style={settingsStyles.arrowIcon}
+                  source={require('../../../assets/images/fleche-blue.png')}
+                />
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              accessibilityLabel="Emplacement"
+              onPress={() => navigation.navigate('Emplacement')}>
+              <View style={settingsStyles.locationItem}>
+                <Text style={settingsStyles.locationText}>Emplacement</Text>
+                <Text style={settingsStyles.locationValue}>Paris, FR</Text>
+                <Image
+                  style={settingsStyles.arrowIcon}
+                  source={require('../../../assets/images/fleche-blue.png')}
+                />
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              accessibilityLabel="Mode invisible"
+              onPress={() => navigation.navigate('Mode invisible')}>
+              <View style={settingsStyles.modeItem}>
+                <View style={settingsStyles.modeContainer}>
+                  <Text style={settingsStyles.modeText}>Mode invisible</Text>
+                  <Text style={settingsStyles.modeDescription}>
+                    Visitez des profils incognito
+                  </Text>
+                </View>
+                <Image
+                  style={settingsStyles.arrowIcon}
+                  source={require('../../../assets/images/fleche-blue.png')}
+                />
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              accessibilityLabel="Mode voyage"
+              onPress={() => navigation.navigate('Mode voyage')}>
+              <View style={settingsStyles.modeItem}>
+                <View style={settingsStyles.modeContainer}>
+                  <Text style={settingsStyles.modeText}>Mode voyage</Text>
+                  <Text style={settingsStyles.modeDescription}>
+                    Pour voir plus de monde. Changez votre localisation.
+                  </Text>
+                </View>
+                <Image
+                  style={settingsStyles.arrowIcon}
+                  source={require('../../../assets/images/fleche-blue.png')}
+                />
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              accessibilityLabel="Mettre mon compte en pause"
+              onPress={() => navigation.navigate('Mettre en pause')}>
+              <View style={settingsStyles.notificationItem}>
+                <Text style={settingsStyles.notificationText}>
+                  Mettre mon compte en pause
+                </Text>
+                <Image
+                  style={settingsStyles.arrowIcon}
+                  source={require('../../../assets/images/fleche-blue.png')}
+                />
+              </View>
+            </TouchableOpacity>
+          </ScrollView>
         </View>
         <View style={settingsStyles.viewBottom}>
           <TouchableOpacity onPress={() => navigation.navigate('Deconnexion')}>
+            <Image
+              style={settingsStyles.logoutButton}
+              source={require('../../../assets/boutons/bouton-deconnexion-blanc.png')}
+            />
+          </TouchableOpacity>
+
+          <Text style={settingsStyles.deleteAccountText}>
+            Supprimer mon compte
+          </Text>
+
           <Image
-            style={settingsStyles.logoutButton}
-            source={require('../../../assets/boutons/bouton-deconnexion-blanc.png')}
+            style={settingsStyles.logoImage}
+            source={require('../../../assets/images/logo-sombre.png')}
           />
-        </TouchableOpacity>
 
-        <Text style={settingsStyles.deleteAccountText}>
-          Supprimer mon compte
-        </Text>
-
-        <Image
-          style={settingsStyles.logoImage}
-          source={require('../../../assets/images/logo-sombre.png')}
-        />
-
-        <Text style={settingsStyles.versionText}>Version 1.0</Text>
+          <Text style={settingsStyles.versionText}>Version 1.0</Text>
         </View>
-        
       </ImageBackground>
     </View>
   );
