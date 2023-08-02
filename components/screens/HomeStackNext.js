77518ead93d2ce62ebb65ba7341042acfcc63eb4
Mi9iNT <72730181+Mi9iNT@users.noreply.github.com>
2023-08-02 10:30:29 +0200

    Ajustement des boutons s'inscrire et se connecter d'homeStackNext.js

--- components/screens/HomeStackNext.js
+++ components/screens/HomeStackNext.js
@@ -28,19 +28,19 @@ export const HomeStackNext = ({navigation}) => {
         </View>
         <View style={[Styles.ViewBtn, {top: -50}]}>
           <TouchableOpacity
-            style={[{top: 0}]}
+            style={[{top: 0, height: 60, width:'90%', alignSelf:'center'}]}
             accessibilityLabel="S'inscrire"
             onPress={() =>
               navigation.navigate('ProfilMe', {routeName: "S'inscrire"})
             }>
-            <Text style={[Styles.textBtn6, {zIndex: 1, top: 45}]}>
+            <Text style={[Styles.textBtn6, {zIndex: 1, top: 10}]}>
               S'inscrire
             </Text>
             <Image
               style={[
                 {
-                  top: 0,
-                  width: '90%',
+                  top: -34,
+                  width: '100%',
                   height: 60,
                   resizeMode: 'contain',
                   alignSelf: 'center',
@@ -50,21 +50,21 @@ export const HomeStackNext = ({navigation}) => {
             />
           </TouchableOpacity>
           <TouchableOpacity
-            style={[{top: -20}]}
+            style={[{top: 20, height: 60, width:'90%', alignSelf:'center'}]}
             accessibilityLabel="Se connecter"
             onPress={() =>
               navigation.navigate('Liens de connexion', {
                 routeName: 'Se connecter',
               })
             }>
-            <Text style={[Styles.textBtn6, {zIndex: 2, top: 55}]}>
+            <Text style={[Styles.textBtn6, {zIndex: 2, top: 10}]}>
               Se connecter
             </Text>
             <Image
               style={[
                 {
-                  top: 10,
-                  width: '90%',
+                  top:-34,
+                  width: '100%',
                   height: 60,
                   resizeMode: 'contain',
                   alignSelf: 'center',
