8c678bd3ccdcd22f18a2e8f8abc12321953ba666
Mi9iNT <72730181+Mi9iNT@users.noreply.github.com>
2023-08-02 12:35:06 +0200

    Add files via upload

---
+++ assets/style/styleScreens/styleSettings/StyleParemetresConfident.js
@@ -0,0 +1,77 @@
+/* eslint-disable prettier/prettier */
+
+const StyleParemetresConfident = {
+  bgGradient: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    top: 30,
+    color: '#0019A7',
+    fontFamily: 'Gilroy',
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 700,
+  },
+  titleItem: {
+    color: '#0019A7',
+    width: 320,
+    textAlign: 'left',
+    fontFamily: 'Gilroy',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: 700,
+    marginBottom: 20,
+  },
+  separator: {
+    backgroundColor: '#0019A7',
+    width: 351,
+    height: 1,
+    alignSelf: 'center',
+    top: 60,
+  },
+  parametresConfidentContainer: {
+    flexShrink: 0,
+    alignSelf: 'center',
+    top: 150,
+    marginBottom: 40,
+  },
+  viewItem: {
+    flexShrink: 0,
+    alignSelf: 'center',
+    top: 150,
+    marginBottom: 40,
+  },
+  textItem: {
+    color: '#929EDE',
+    width: 260,
+    alignSelf: 'flex-start',
+    textAlign: 'left',
+    fontFamily: 'Gilroy',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: 700,
+  },
+ 
+  backButton: {
+    top: 220,
+    width: 331,
+    height: 56,
+    flexShrink: 0,
+    alignSelf: 'center',
+  },
+  backButtonText: {
+    top: 180,
+    left: 10,
+    color: '#0019A7',
+    textAlign: 'center',
+    alignSelf: 'center',
+    fontFamily: 'Comfortaa',
+    fontSize: 18,
+    fontStyle: 'normal',
+    fontWeight: 700,
+  },
+};
+
+export default StyleParemetresConfident;
